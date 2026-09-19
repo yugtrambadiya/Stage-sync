e5a0832ee6a52358eb81f53377579071f676a6c3
Brijesh-Zalavadiya <zalvadiyabrijesh135@gmail.com>
2026-09-19 15:36:12 +0530

    Changes in the index.ts are pushed

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -5,8 +5,7 @@ declare global {
   var __prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  global.__prisma ??
+export const prisma = global.__prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
   });
